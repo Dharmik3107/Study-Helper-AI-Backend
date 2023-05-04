3dafbdd605fb80ebc4e5ef48a1e26ef8a2cfde08
Dharmik Abhangi <abhangidharmik@gmail.com>
2023-05-04 11:26:47 +0530

    CORS Updation

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: ["http://localhost:5173", "https://chat-study-helper.netlify.app"] }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
